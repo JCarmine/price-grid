34a9cf23b301e38f1c34bd54e596fcfa44fdc482
Jesse Carmine <jessecarmine@jcarmine-mbp-bos.local>
2016-11-30 17:02:24 -0500

    All 3 values storing correctly.

--- PricingGrid.js
+++ PricingGrid.js
@@ -5,7 +5,7 @@
 
 var conditionNames  = ["fair", "good", "excellent"];
 var conditionValues = [100, 110, 120];
-var conditionPrices = [444, 555, 120];
+var conditionPrices = [222, 555, 777];
 
 var colorNames  = ["black", "white", "purple", "blue"];
 var colorValues = [52, 64, 77, 90];
@@ -19,18 +19,22 @@ var excellentStock = [110, 120, 90];
 	var numberOfItems = conditionNames.length * colorNames.length;
   var columnCount = conditionNames.length;
   var columnIndex = 0;
+  var rowCount = colorNames.length;
+  var rowIndex = 0;
   
   for (i = 0; i < numberOfItems; i++) {
   	alert(columnIndex);
     var li = document.createElement("li");
-    li.innerHTML = conditionValues[columnIndex];
+    li.innerHTML = conditionValues[columnIndex] + " " + colorValues[rowIndex] 
+    + " " + conditionPrices[columnIndex];
     document.getElementById("main").appendChild(li);
     columnIndex++;
     
-    if (columnIndex === columnCount) {
-    	columnIndex = 0;
+    if (columnIndex == columnCount) {
+      rowIndex++;
+      columnIndex = 0;
     }
-
+   
 	}
  
 })();
