e4eff6f0b2038593918e0e562f4de1f970bf1515
Jesse Carmine <jessecarmine@jcarmine-mbp-bos.local>
2016-11-30 15:34:15 -0500

    Basic items list.

---
+++ PricingGrid.js
@@ -0,0 +1,27 @@
+<body>
+<ul id="main"></ul>
+</body>
+
+
+var conditionNames  = ["fair", "good", "excellent"];
+var conditionValues = [100, 110, 120];
+var conditionPrices = [444, 555, 120];
+
+var colorNames  = ["black", "white", "purple", "blue"];
+var colorValues = [52, 64, 77, 90];
+
+var fairStock      = [100, 110, 120, 52, 64];
+var goodStock      = [100, 110, 120, 52, 64, 77, 90];
+var excellentStock = [110, 120, 90];
+
+(function(){
+	var numberOfItems = 3 * 4;
+  alert(numberOfItems);
+  
+  for (i = 0; i <= numberOfItems; i++) { 
+    var li = document.createElement("li");
+    li.innerHTML = "blue"
+    document.getElementById("main").appendChild(li);
+	}
+ 
+})();
