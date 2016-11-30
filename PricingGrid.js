a487b532cce27024cb1ab2ea7441e51e8845f768
Jesse Carmine <jessecarmine@jcarmine-mbp-bos.local>
2016-11-30 17:46:38 -0500

    Re-organize array.

--- PricingGrid.js
+++ PricingGrid.js
@@ -2,6 +2,15 @@
 <ul id="main"></ul>
 </body>
 
+#main {
+  width: 100%;
+}
+
+li {
+  display: inline-block;
+  width: 33%;
+}
+
 
 var conditionNames  = ["fair", "good", "excellent"];
 var conditionValues = [100, 110, 120];
@@ -10,11 +19,11 @@ var conditionPrices = [222, 555, 777];
 var colorNames  = ["black", "white", "purple", "blue"];
 var colorValues = [52, 64, 77, 90];
 
-var fairStock      = [100, 110, 120, 52, 64];
-var goodStock      = [100, 110, 120, 52, 64, 77, 90];
-var excellentStock = [110, 120, 90];
-
-
+var stockLevels = [  [100, 110, 120, 52, 64]
+                    ,[100, 110, 120, 52, 64, 77, 90]
+                    ,[110, 120, 90]
+                  ];
+alert(stockLevels[0].length);
 (function(){
 	var numberOfItems = conditionNames.length * colorNames.length;
   var columnCount = conditionNames.length;
@@ -23,10 +32,13 @@ var excellentStock = [110, 120, 90];
   var rowIndex = 0;
   
   for (i = 0; i < numberOfItems; i++) {
-  	alert(columnIndex);
+
     var li = document.createElement("li");
     li.innerHTML = conditionValues[columnIndex] + " " + colorValues[rowIndex] 
     + " " + conditionPrices[columnIndex];
+    
+   
+   
     document.getElementById("main").appendChild(li);
     columnIndex++;
     
